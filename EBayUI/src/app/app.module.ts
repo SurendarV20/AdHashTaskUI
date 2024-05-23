67ebc16d5c3d9b0948cae22628a3b7c5e7c52234
Surendar <surendar@designpluz.com>
2024-05-23 23:59:15 +0530

    .

--- EBayUI/src/app/app.module.ts
+++ EBayUI/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent],
